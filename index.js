775c1b434ed3c9927558169c4c0a06cc4b75e31d
betester <ikramzulkarnaen02@gmail.com>
2022-07-01 23:59:28 +0700

    add initial response

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const token = process.env.DISCORD_TOKEN
 
+console.log(token,"index js");
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -9,5 +11,18 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
 // Login to Discord with your client's token
 client.login(token);
