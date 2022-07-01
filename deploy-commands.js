8a54afa0fb008a757380d231afc5107e15812f88
betester <ikramzulkarnaen02@gmail.com>
2022-07-01 23:46:28 +0700

    change node version to 16

---
+++ deploy-commands.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+// required data 
+const guildId = process.env.GUILD_ID
+const clientId = process.env.CLIENT_ID
+const token = process.env.DISCORD_TOKEN
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
