ad64be62234fd558eeb7c219d77625b0d80d2d28
betester <ikramzulkarnaen02@gmail.com>
2022-07-02 00:55:56 +0700

    check for console logs on project

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,7 +10,7 @@ const token = process.env.DISCORD_TOKEN;
 
 
 
-console.log(token,"deploy-commands");
+// console.log(token,"deploy-commands");
 
 const commands = [
   new SlashCommandBuilder()
@@ -28,5 +28,5 @@ const rest = new REST({ version: "9" }).setToken(token);
 
 rest
   .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-  .then(() => console.log("Successfully registered application commands."))
-  .catch(console.error);
+  .then(() => {})
+  .catch();
