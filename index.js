e89cf089254042dbcb598cebf7d0c54f2dbd46b1
betester <ikramzulkarnaen02@gmail.com>
2022-07-02 00:26:10 +0700

    testing husky

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-require('dotenv').config();
-const { Client, Intents } = require('discord.js');
-const token = process.env.DISCORD_TOKEN
+require("dotenv").config();
+const { Client, Intents } = require("discord.js");
+const token = process.env.DISCORD_TOKEN;
 
-console.log(token,"index js");
+console.log(token, "index js");
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+console.log("asdad");
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+  const { commandName } = interaction;
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
-	}
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply("Server info.");
+  } else if (commandName === "user") {
+    await interaction.reply("User info.");
+  }
 });
 // Login to Discord with your client's token
 client.login(token);
