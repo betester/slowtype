886a73e55d83c05d8839bf3fd122ac03703917ed
betester <ikramzulkarnaen02@gmail.com>
2022-07-02 00:54:19 +0700

    check for console logs on project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,15 @@
 module.exports = {
-  env: {
-    node: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
-  rules: {
-    "no-console": "off",
-    "no-restricted-syntax": [
-      "error",
-      {
-        selector:
-          "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
-        message: "Unexpected property on console object was called",
-      },
-    ],
-  },
-};
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-console" : 1,
+        
+    }
+}
